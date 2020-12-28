7d15b60898571cc21e9d524f8ae5f3ac1545d772
赵董 <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2020-12-28 15:54:32 +0800

    修改端口号 差生成报告

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -42,6 +42,8 @@ import echarts from 'echarts'
 export default {
   data () {
     return {
+      arry: [],
+      obj: {},
       firstBox: null,
       secondBox: null,
       thirdBox: null,
@@ -141,13 +143,14 @@ export default {
             },
             xAxis: {
               type: 'value',
+              boundaryGap: [0, 0.1],
+              minInterval: 1,
               axisLabel: {
                 formatter: '{value} 台次'
               }
             },
             yAxis: {
               type: 'category',
-              // eslint-disable-next-line no-undef
               data: data.placeBlackoutDtos.map(item => {
                 return item.placeName
               })
@@ -193,9 +196,16 @@ export default {
         url: this.$http.adornUrl('/app/repeatDetailed/reasonCensus'),
         method: 'get'
       }).then(({data}) => {
+        this.obj = {}
+        this.arry = []
+        data.reasonCensusDtos.map(item => {
+          delete data.reasonCensusDtos['percentage']
+          this.arry.push({
+            value: item.cateCount,
+            name: item.category
+          })
+        })
         if (data && data.code === 0) {
-          console.log(data)
-
           var option = {
             title: {
               text: '停电原因统计',
@@ -233,15 +243,7 @@ export default {
                 labelLine: {
                   show: false
                 },
-                data: data.reasonCensusDtos.forEach((item, index, array) => {
-                  delete data.reasonCensusDtos['percentage']
-                  var arry = []
-                  var obj = {}
-                  obj['name'] = item.category
-                  obj['value'] = item.cateCount
-                  arry.push(obj)
-                  return array
-                })
+                data: this.arry
               }
             ]
           }
@@ -255,192 +257,199 @@ export default {
     },
     // 停电报送核查统计 柱状图统计
     initThirdBox () {
-      var option = {
-        title: {
-          text: '停电报送核查统计',
-          subtext: '柱状图统计“停电明细导入分析”菜单中60天内各供电所上报停电记录的匹配率。匹配度低的在前。'
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'shadow'
+      this.$http({
+        url: this.$http.adornUrl('/app/repeatDetailed/placeMatching'),
+        method: 'get'
+      }).then(({data}) => {
+        if (data && data.code === 0) {
+          var option = {
+            title: {
+              text: '停电报送核查统计',
+              subtext: '柱状图统计“停电明细导入分析”菜单中60天内各供电所上报停电记录的匹配率。匹配度低的在前。'
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                type: 'shadow'
+              }
+            },
+            legend: {
+              data: ['匹配率']
+            },
+            grid: {
+              left: '3%',
+              right: '4%',
+              bottom: '3%',
+              containLabel: true
+            },
+            xAxis: [
+              {
+                type: 'category',
+                data: data.placeMatchingDtos.map(item => {
+                  return item.placeName
+                })
+              }
+            ],
+            yAxis: [
+              {
+                type: 'value',
+                boundaryGap: [0, 0.1]
+              }
+            ],
+            series: [
+              {
+                name: '匹配率',
+                type: 'bar',
+                data: data.placeMatchingDtos.map(item => {
+                  return item.matching
+                })
+              }
+            ]
           }
-        },
-        legend: {
-          data: ['匹配率']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: ['供电所1', '供电所2', '供电所3', '供电所4', '供电所5', '供电所6', '供电所7', '供电所8', '供电所9', '供电所10', '供电所11', '供电所12', '供电所13', '供电所14']
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: '匹配率',
-            type: 'bar',
-            data: [0.12, 0.33, 0.12, 0.21, 0.36, 0.45, 0.33, 0.12, 0.11, 0.01, 0.03, 0.01, 0.03, 0.22]
-          }
-        ]
-      }
-      this.thirdBox = echarts.init(document.getElementById('J_thirdBox'))
-      this.thirdBox.setOption(option)
-      window.addEventListener('resize', () => {
-        this.thirdBox.resize()
+          this.thirdBox = echarts.init(document.getElementById('J_thirdBox'))
+          this.thirdBox.setOption(option)
+          window.addEventListener('resize', () => {
+            this.thirdBox.resize()
+          })
+        }
       })
     },
     // 台区经理重复停电统计
     initFourthBox () {
-      var option = {
-        title: {
-          text: '台区经理重复停电统计',
-          subtext: '横向柱状图统计“停电明细导入分析”菜单中60天内各台区经理的停电次数，数量多的在上。'
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'shadow'
+      this.$http({
+        url: this.$http.adornUrl('/app/repeatDetailed/districtManager'),
+        method: 'get'
+      }).then(({data}) => {
+        if (data && data.code === 0) {
+          var option = {
+            title: {
+              text: '台区经理重复停电统计',
+              subtext: '横向柱状图统计“停电明细导入分析”菜单中60天内各台区经理的停电次数，数量多的在上。'
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                type: 'shadow'
+              }
+            },
+            legend: {
+              data: ['停电次数']
+            },
+            grid: {
+              left: '3%',
+              right: '4%',
+              bottom: '3%',
+              containLabel: true
+            },
+            xAxis: {
+              type: 'value',
+              boundaryGap: [0, 0.01]
+            },
+            yAxis: {
+              type: 'category',
+              data: data.districtManagerDtos.map(item => {
+                return item.managerName
+              })
+            },
+            series: [
+              {
+                name: '停电次数',
+                type: 'bar',
+                data: data.districtManagerDtos.map(item => {
+                  return item.blackoutCount
+                })
+              }
+            ]
           }
-        },
-        legend: {
-          data: ['停电次数']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: {
-          type: 'value',
-          boundaryGap: [0, 0.01]
-        },
-        yAxis: {
-          type: 'category',
-          data: ['经理1', '经理2', '经理3', '经理4', '经理5', '经理6', '经理7', '经理8', '经理9', '经理10']
-        },
-        series: [
-          {
-            name: '停电次数',
-            type: 'bar',
-            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-          }
-        ]
-      }
-      this.fourthBox = echarts.init(document.getElementById('J_fourthBox'))
-      this.fourthBox.setOption(option)
-      window.addEventListener('resize', () => {
-        this.fourthBox.resize()
+          this.fourthBox = echarts.init(document.getElementById('J_fourthBox'))
+          this.fourthBox.setOption(option)
+          window.addEventListener('resize', () => {
+            this.fourthBox.resize()
+          })
+        }
       })
     },
 
     initFifthBox () {
-      var option = {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        legend: {
-          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎', '百度', '谷歌', '必应', '其他']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            name: '直接访问',
-            type: 'bar',
-            data: [320, 332, 301, 334, 390, 330, 320]
-          },
-          {
-            name: '邮件营销',
-            type: 'bar',
-            stack: '广告',
-            data: [120, 132, 101, 134, 90, 230, 210]
-          },
-          {
-            name: '联盟广告',
-            type: 'bar',
-            stack: '广告',
-            data: [220, 182, 191, 234, 290, 330, 310]
-          },
-          {
-            name: '视频广告',
-            type: 'bar',
-            stack: '广告',
-            data: [150, 232, 201, 154, 190, 330, 410]
-          },
-          {
-            name: '搜索引擎',
-            type: 'bar',
-            data: [862, 1018, 964, 1026, 1679, 1600, 1570],
-            markLine: {
-              lineStyle: {
-                type: 'dashed'
-              },
-              data: [
-                [{type: 'min'}, {type: 'max'}]
-              ]
+      this.$http({
+        url: this.$http.adornUrl('/app/repeatDetailed/correctiveList'),
+        method: 'get'
+      }).then(({data}) => {
+        if (data && data.code === 0) {
+          for (let i = 0; i < data.correctiveActionDtos.length; i++) {
+            for (let j = 1; j < data.correctiveActionDtos.length; j++) {
+              if (data.correctiveActionDtos[i].correctiveRate > data.correctiveActionDtos[j].correctiveRate) {
+                const arrs = data.correctiveActionDtos[i]
+                data.correctiveActionDtos[i] = data.correctiveActionDtos[j]
+                data.correctiveActionDtos[j] = arrs
+              }
             }
-          },
-          {
-            name: '百度',
-            type: 'bar',
-            barWidth: 5,
-            stack: '搜索引擎',
-            data: [620, 732, 701, 734, 1090, 1130, 1120]
-          },
-          {
-            name: '谷歌',
-            type: 'bar',
-            stack: '搜索引擎',
-            data: [120, 132, 101, 134, 290, 230, 220]
-          },
-          {
-            name: '必应',
-            type: 'bar',
-            stack: '搜索引擎',
-            data: [60, 72, 71, 74, 190, 130, 110]
-          },
-          {
-            name: '其他',
-            type: 'bar',
-            stack: '搜索引擎',
-            data: [62, 82, 91, 84, 109, 110, 120]
           }
-        ]
-      }
-      this.fifthBox = echarts.init(document.getElementById('J_fifthBox'))
-      this.fifthBox.setOption(option)
-      window.addEventListener('resize', () => {
-        this.fifthBox.resize()
+          var option = {
+            title: {
+              text: '重复停电整改统计',
+              subtext: '堆叠柱状图统计“停电明细导入分析”菜单中60天内的各供电所重复停电闭环情况。'
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+              }
+            },
+            legend: {
+              data: ['未整改', '进行中', '已整改']
+            },
+            grid: {
+              left: '3%',
+              right: '4%',
+              bottom: '3%',
+              containLabel: true
+            },
+            xAxis: [
+              {
+                type: 'category',
+                data: data.correctiveActionDtos.map(item => {
+                  return item.placeName
+                })
+              }
+            ],
+            yAxis: [
+              {
+                type: 'value',
+                boundaryGap: [0, 0.1],
+                minInterval: 1
+              }
+            ],
+            series: [
+              {
+                name: '未整改',
+                type: 'bar',
+                data: data.correctiveActionDtos.map(item => {
+                  return item.noCorrectiveCount
+                })
+              },
+              {
+                name: '进行中',
+                type: 'bar',
+                data: data.correctiveActionDtos.map(item => {
+                  return item.carryOnCount
+                })
+              },
+              {
+                name: '已整改',
+                type: 'bar',
+                data: data.correctiveActionDtos.map(item => {
+                  return item.correctiveCount
+                })
+              }
+            ]
+          }
+          this.fifthBox = echarts.init(document.getElementById('J_fifthBox'))
+          this.fifthBox.setOption(option)
+          window.addEventListener('resize', () => {
+            this.fifthBox.resize()
+          })
+        }
       })
     }
 
--- static/config/index.js
+++ static/config/index.js
@@ -5,9 +5,8 @@
   window.SITE_CONFIG = {}
 
   // api接口请求地址
-  // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast'
-  window.SITE_CONFIG['baseUrl'] = 'http://powercut.free.idcfengye.com/renren-fast'
-
+  window.SITE_CONFIG['baseUrl'] = 'http://101.132.138.101:8080/renren-fast'
+  // window.SITE_CONFIG['baseUrl'] = 'http://powercut.free.idcfengye.com/renren-fast'
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
